6e6fbc73269b68ae66348c41769059db01a720c7
edarsh7 <edarsh7@gmail.com>
2021-01-13 13:47:42 -0800

    timer.c modified

--- CSE130/Lab1/pintos/src/devices/timer.c
+++ CSE130/Lab1/pintos/src/devices/timer.c
@@ -101,9 +101,9 @@ timer_sleep (int64_t ticks)
 {
   int64_t start = timer_ticks ();
 
-  ASSERT (intr_get_level () == INTR_ON);
-  while (timer_elapsed (start) < ticks) 
-  thread_yield ();
+  intr_disable();
+  thread_block();
+  intr_enable();
 }
 
 /* 
